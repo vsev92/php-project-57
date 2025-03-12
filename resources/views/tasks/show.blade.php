65de2791df1e967eb54a3ba3673e685ae1f30a12
vsev92 <vsev92@gmail.com>
2025-03-12 13:43:30 +0700

    add localization

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -4,7 +4,7 @@
     <div class="grid max-w-screen-xl px-4 pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:pt-28">
         <div class="grid col-span-full">
             <h2 class="mb-5">
-                Просмотр задачи: {{$task->name}} <a href="{{route('task.edit',$task->id)}}">&#9881;</a>
+                Просмотр задачи: {{$task->name}} <a href="{{route('tasks.edit',$task->id)}}">&#9881;</a>
                 </h1>
                 <p><span class="font-black">Имя:</span> {{$task->name}}</p>
                 <p><span class="font-black">Статус:</span> {{$task->status->name}}</p>
