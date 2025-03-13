76adc1e64dc1288a2277ece821f736c4ca753ba9
vsev92 <vsev92@gmail.com>
2025-03-13 14:40:16 +0700

    modify validation message3

--- app/Http/Controllers/Auth/ConfirmablePasswordController.php
+++ app/Http/Controllers/Auth/ConfirmablePasswordController.php
@@ -24,13 +24,8 @@ class ConfirmablePasswordController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        if (! Auth::guard('web')->validate([
-            'email' => $request->user()->email,
-            'password' => $request->password,
-        ])) {
-            throw ValidationException::withMessages([
-                'password' => __('auth.password'),
-            ]);
+        if (!Auth::guard('web')->validate(['email' => $request->user()->email, 'password' => $request->password])) {
+            throw ValidationException::withMessages(['password' => ('auth.password')]);
         }
 
         $request->session()->put('auth.password_confirmed_at', time());
--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -53,7 +53,6 @@ class RegisteredUserController extends Controller
     */
 
     public function messages()
-
     {
 
         return [
