693d704b27aa32977fc700eb8724d59d02b8d37e
vsev92 <vsev92@gmail.com>
2025-03-18 11:06:12 +0700

    stan fix 9

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TaskStatusController;
 use App\Http\Controllers\LabelController;
@@ -10,16 +9,8 @@ Route::get('/', function () {
     return view('greetFromHexlet');
 })->name('home');
 
-
 Route::resource('tasks', TaskController::class);
 Route::resource('task_statuses', TaskStatusController::class);
 Route::resource('labels', LabelController::class);
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__ . '/auth.php';
