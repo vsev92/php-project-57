9ce7b47e2bbac35ce5889cd0da928a430a89abde
vsev92 <vsev92@gmail.com>
2025-03-24 14:18:22 +0700

    modify templates2

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -37,7 +37,7 @@
                     </form>
                 </div>
             </div>
-            @can('store', $task)
+            @can('store', App\Models\Task::class)
             <div class="ml-auto">
                 <a href="{{route('tasks.create')}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2">
                     Создать задачу </a>
